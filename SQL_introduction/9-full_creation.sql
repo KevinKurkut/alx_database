4bdda9e4b95169962f69d9d970e7b83ec554f142
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-08-18 22:24:18 +0300

    update

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,13 +1,12 @@
 -- Full creation
 USE hbtn_0c_0;
--- Create new table
 CREATE TABLE IF NOT EXISTS second_table (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256),
     score INT
 );
 -- insert records
-INSERT INTO second_table( id, name, score) VALUES ("John", 10);
-INSERT INTO second_table( id, name, score) VALUES ("Alex", 3);
-INSERT INTO second_table( id, name, score) VALUES ("Bob", 14);
-INSERT INTO second_table( id, name, score) VALUES ("George", 8);
+INSERT INTO second_table( id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table( id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table( id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table( id, name, score) VALUES (4, "George", 8);
