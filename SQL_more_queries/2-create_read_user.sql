706e46e276aa42cf2d104b5e4a78d2ea0c4c88a3
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-08-20 18:14:01 +0300

    read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,3 @@
--- Create Read User
+-- Create read user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
--- grant read
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
